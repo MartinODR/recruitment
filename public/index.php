a9ba3b0689e2099ee177cc60bf0163637a6309fb
MartinOdr <martinsteinturm@gmail.com>
2024-04-18 17:29:19 +0200

    "Add implementation of fast-route library from nikik"

--- public/index.php
+++ public/index.php
@@ -7,14 +7,53 @@
 error_reporting(error_level:E_ALL);
 ini_set(option:'display_errors', value: '1');
 
-echo'Hallo 2306 / -7';
+//echo'Hallo 2306 / -7';
 
 
 //Autoloading
 require_once __DIR__ . '/../vendor/autoload.php';
 
 //codigo temporal
-$controller = new \App\Controller\HomeController(); //esto crea un objeto llamado desde el controlador(HomeController) y llama al metodo index()
-$controller->index();
+//$controller = new \App\Controller\HomeController(); //esto crea un objeto llamado desde el controlador(HomeController) y llama al metodo index()
+//$controller->index();
 
+//https://github.com/nikic/FastRoute   composer require nikic/fast-route
 
+//Fast-Route-Library
+
+//dispatcher einrichten
+$dispatcher = FastRoute\simpleDispatcher(function(FastRoute\RouteCollector $r) {
+    $r->addRoute('GET', '/', 'App\Controller\HomeController::index');   //das ist der handler
+    // {id} must be a number (\d+)
+   // $r->addRoute('GET', '/user/{id:\d+}', 'get_user_handler');
+    // The /{title} suffix is optional
+   // $r->addRoute('GET', '/articles/{id:\d+}[/{title}]', 'get_article_handler');
+});
+
+//HTTP-Methode und URI abrufen
+$httpMethod = $_SERVER['REQUEST_METHOD'];
+$uri = $_SERVER['REQUEST_URI'];
+
+// Strip query string (?foo=bar) and decode URI  Unnötige URL-Teile enfernen 
+if (false !== $pos = strpos($uri, '?')) {
+    $uri = substr($uri, 0, $pos);
+}
+$uri = rawurldecode($uri);
+
+//routing abgleichen 
+
+$routeInfo = $dispatcher->dispatch($httpMethod, $uri);
+switch ($routeInfo[0]) {
+    case FastRoute\Dispatcher::NOT_FOUND:
+        // ... 404 Not Found
+        break;
+    case FastRoute\Dispatcher::METHOD_NOT_ALLOWED:
+        $allowedMethods = $routeInfo[1];
+        // ... 405 Method Not Allowed
+        break;
+    case FastRoute\Dispatcher::FOUND:
+        $handler = $routeInfo[1];
+        $vars = $routeInfo[2];
+        // ... call $handler with $vars
+        break;
+}
--- vendor/composer/installed.php
+++ vendor/composer/installed.php
@@ -1,9 +1,9 @@
 <?php return array(
     'root' => array(
         'name' => '__root__',
-        'pretty_version' => '1.0.0+no-version-set',
-        'version' => '1.0.0.0',
-        'reference' => null,
+        'pretty_version' => 'dev-main',
+        'version' => 'dev-main',
+        'reference' => 'e272346aef2407dbcc0429de483e08d2cfc809b3',
         'type' => 'library',
         'install_path' => __DIR__ . '/../../',
         'aliases' => array(),
@@ -11,11 +11,20 @@
     ),
     'versions' => array(
         '__root__' => array(
-            'pretty_version' => '1.0.0+no-version-set',
-            'version' => '1.0.0.0',
-            'reference' => null,
+            'pretty_version' => 'dev-main',
+            'version' => 'dev-main',
+            'reference' => 'e272346aef2407dbcc0429de483e08d2cfc809b3',
             'type' => 'library',
             'install_path' => __DIR__ . '/../../',
+            'aliases' => array(),
+            'dev_requirement' => false,
+        ),
+        'nikic/fast-route' => array(
+            'pretty_version' => 'v1.3.0',
+            'version' => '1.3.0.0',
+            'reference' => '181d480e08d9476e61381e04a71b34dc0432e812',
+            'type' => 'library',
+            'install_path' => __DIR__ . '/../nikic/fast-route',
             'aliases' => array(),
             'dev_requirement' => false,
         ),
